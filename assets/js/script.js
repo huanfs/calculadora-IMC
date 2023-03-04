dc9064cecf00078825a943c11c447bdedcfafd63
Ruan Figuereido <huanfiguereido@gmail.com>
2023-03-04 20:25:00 -0300

    refatoração de código
    refatoração do javascript, ajuste das funções e simplificação da lógica.
    melhoria visual, ajuste de código css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,55 @@
-let altura=document.querySelector('input#altura')
-let peso=document.querySelector('input#peso')
+let altura=document.querySelector('input#altura');
+let peso=document.querySelector('input#peso');
+let res=document.getElementsByTagName('article')[0];
 
 function calcular(){
     let alt2=(Number(altura.value)*Number(altura.value));
     let imc=(Number(peso.value)/alt2);
-
+    //ESTILIZAÇÕES PADRÕES DA CAIXA DE RESULTADO
+    res.style.fontSize='18pt';
+    res.style.width='100%';
+    res.style.height='90%';
+    res.style.borderRadius='5px';
+    res.style.fontFamily='Arial';
+    res.style.padding='10px';
+    res.style.color='#000';
+    res.style.textAlign='justify';
+    //ESTILIZAÇÕES ALTERNATIVAS DA CAIXA DE RESULTADOS
+    if(imc<18.5||imc>30&&imc<34.9||imc>35&&imc<39.9){
+        res.style.backgroundColor='rgb(235, 53, 16)';
+    }
+    else if(imc>18.6&&imc<24.9||imc>25&&imc<29.9){
+        res.style.backgroundColor='rgb(33, 236, 57)';
+    }
+    //PROCESSAMENTO E ESCRITA DOS RESULTADOS
     if(imc<18.5){
-        let res=document.getElementsByTagName('article')[0];
-        res.innerHTML=`Seu índice de massa corporal (IMC) é de ${imc.toFixed(1)}, Caracterizando Extrema magreza`;
-        res.style.color='#E7720B';
-        res.style.textAlign='center';
-        res.style.backgroundColor='#E9F7F0';
-        res.style.fontSize='18pt';
-        res.style.marginTop='2em';
-        res.style.width='340px';
-        res.style.fontFamily='Arial';
-        res.style.padding='10px';
+        res.innerHTML=`Seu índice de massa corporal (IMC) é de ${imc.toFixed(1)}, Caracterizando Extrema magreza, Procure ajuda Médica!`;
+        res.style.backgroundColor='#000';
+        res.style.color='#fff';   
     }
     else if(imc>18.6&&imc<24.9){
-        let res=document.getElementsByTagName('article')[0];
         res.innerHTML=`Seu índice de massa corporal (IMC) é de ${imc.toFixed(1)}, Parabens, você está no peso ideal`;
-        res.style.color='#77C731';
-        res.style.textAlign='center';
-        res.style.backgroundColor='#E9F7F0';
-        res.style.fontSize='18pt';
-        res.style.marginTop='2em';
-        res.style.width='340px';
-        res.style.fontFamily='Arial';
-        res.style.padding='10px';
     }
     else if(imc>25&&imc<29.9){
-        let res=document.getElementsByTagName('article')[0];
         res.innerHTML=`Seu índice de massa corporal (IMC) é de ${imc.toFixed(1)}, Você está levemente acima do peso`;
-        res.style.color='#77C731';
-        res.style.textAlign='center';
-        res.style.backgroundColor='#E9F7F0';
-        res.style.fontSize='18pt';
-        res.style.marginTop='2em';
-        res.style.width='340px';
-        res.style.fontFamily='Arial';
-        res.style.padding='10px';
     }
     else if(imc>30&&imc<34.9){
-        let res=document.getElementsByTagName('article')[0];
         res.innerHTML=`Seu índice de massa corporal (IMC) é de ${imc.toFixed(1)}, Caracterizando obesidade de grau 1`;
-        res.style.color='#E7720B';
-        res.style.textAlign='center';
-        res.style.backgroundColor='#E9F7F0';
-        res.style.fontSize='18pt';
-        res.style.marginTop='2em';
-        res.style.width='340px';
-        res.style.fontFamily='Arial';
-        res.style.padding='10px';
     }
     else if(imc>35&&imc<39.9){
-        let res=document.getElementsByTagName('article')[0];
         res.innerHTML=`Seu índice de massa corporal (IMC) é de ${imc.toFixed(1)}, Caracterizando obesidade de grau 2(Severa)`;
-        res.style.color='#E7720B';
-        res.style.textAlign='center';
-        res.style.backgroundColor='#E9F7F0';
-        res.style.fontSize='18pt';
-        res.style.marginTop='2em';
-        res.style.width='340px';
-        res.style.fontFamily='Arial';
-        res.style.padding='10px';
     }
     else if(imc>40){
-        let res=document.getElementsByTagName('article')[0];
         res.innerHTML=`Seu índice de massa corporal (IMC) é de ${imc.toFixed(1)}, Caracterizando obesidade de grau 3(Mórbida),Procure ajuda médica!`;
-        res.style.color='#E7720B';
-        res.style.textAlign='center';
-        res.style.backgroundColor='#E9F7F0';
-        res.style.fontSize='18pt';
-        res.style.marginTop='2em';
-        res.style.width='340px';
-        res.style.fontFamily='Arial';
-        res.style.padding='10px';
+        res.style.backgroundColor='#000';
+        res.style.color="#fff";
     }
 }
-
+//BOTÃO LIMPAR
 function limpar(){
-	let res=document.getElementsByTagName('article')[0];
+	// let res=document.getElementsByTagName('article')[0];
     altura.value=null;
     peso.value=null;
     res.innerHTML=null;
-    res.style.backgroundColor='#BDF8E4';
+    res.style.backgroundColor='#fff';
 }
